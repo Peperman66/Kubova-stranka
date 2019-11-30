681acc679cc1f979d00fb35d11b9eb5092fd6a08
Peperman66 <peperman66@seznam.cz>
2019-11-30 20:09:11 +0100

    add status codes to http responses
    Added status codes to http responses in timers router

--- routers/timers.js
+++ routers/timers.js
@@ -9,7 +9,7 @@ router.all('/', (req, res) => {
     } else if (req.method === 'post') {
 
     } else {
-        res.status(405).json({ error: config.errorMessages.timerAPI.methodNotAllowed });
+        res.status(405).json({ statusCode: 405, error: config.errorMessages.timerAPI.methodNotAllowed });
     }
 });
 
@@ -20,15 +20,15 @@ router.all('/:timer', (req, res) => {
     if (req.method === 'GET') {
         db = sqlite3.Database(config.dbDirectory + 'timers.sqlite3', sqlite3.OPEN_READWRITE, (err) => {
             if (err) {
-                res.status(500).json({ error: err.message });
+                res.status(500).json({ statusCode: 500, error: err.message });
             }
         });
         let query = sql`SELECT id, name, title, header, footer, enddate, expirydate FROM timers WHERE ? in (id, name);`;
         db.get(query, [timer], (err, row) => {
             if (err) {
-                res.status(500).json({ error: err.message });
+                res.status(500).json({ statusCode: 500, error: err.message });
             } else if (!row) {
-                res.status(404).json({ error: config.errorMessages.timerAPI.notFound});
+                res.status(404).json({ statusCode: 404, error: config.errorMessages.timerAPI.notFound});
             } else if (Date.parse(row.expiryDate) > Date.now()){
                 let deleteQuery = sql`DELETE FROM timers WHERE id IS ?;`
                 db.run(deleteQuery, [row.id], (err) => {
@@ -36,7 +36,7 @@ router.all('/:timer', (req, res) => {
                         res.status(500).json({ error: err.message });
                     }
                 });
-                res.status(404).json({ error: config.errorMessages.timerAPI.expired});
+                res.status(404).json({ statusCode: 404, error: config.errorMessages.timerAPI.expired});
             } else {
                 res.status(200).json(row);
             }
@@ -49,11 +49,11 @@ router.all('/:timer', (req, res) => {
     } else if (req.method === 'DELETE') {
         
     } else {
-        res.status(405).json({error: config.errorMessages.timerAPI.methodNotAllowed});
+        res.status(405).json({ statusCode: 405, error: config.errorMessages.timerAPI.methodNotAllowed});
     }
     db.close((err) => {
         if (err) {
-            res.status(500).json({ error: err.message });
+            res.status(500).json({ statusCode: 500, error: err.message });
         }
     }
 });
