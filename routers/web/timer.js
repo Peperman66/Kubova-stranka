13d6ed37316ffe7c2335279b8e383f8f024010b0
Peperman66 <peperman66@seznam.cz>
2019-12-02 18:21:09 +0100

    add rest of websites for timers
    Added rest of websites for timers

--- routers/web/timer.js
+++ routers/web/timer.js
@@ -2,8 +2,20 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../web/dynamic/timers/viewTimers.html'));
+});
+
 router.get('/:timer', (req, res) => {
     res.sendFile(path.join(__dirname, '../../web/dynamic/timers/viewTimer.html'));
+});
+
+router.get('/:timer/update', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../web/dynamic/timers/updateTimer.html'));
+});
+
+router.get('/:timer/delete', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../web/dynamic/timers/deleteTimer.html'));
 });
 
 module.exports = router;
