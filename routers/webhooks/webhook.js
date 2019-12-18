d4aec51db839e3acd96e334e5e7841df909daae6
Peperman66 <peperman66@seznam.cz>
2019-12-18 19:39:42 +0100

    add http request to discord in webhook api
    Added http request to discord in webhook api

--- routers/webhooks/webhook.js
+++ routers/webhooks/webhook.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const https = require('https');
 
 router.post('/:webhookId/:webhookToken/:service', (req, res) => {
-    const discordAddress = `https://discordapp.com/api/webhooks/${req.params.webhookId}/${req.params.webhookToken}`;
+    const discordAddress = `/api/webhooks/${req.params.webhookId}/${req.params.webhookToken}`;
     const service = req.params.service;
     const body = req.body;
     if (service.toLowerCase() === 'trello') {
-        let embed = {
+        let webhookBody = {
             username: 'Trello',
             embeds: [
                 {
@@ -21,10 +22,29 @@ router.post('/:webhookId/:webhookToken/:service', (req, res) => {
             ]
         }
         if (body.action.type === 'addLabelToCard') {
-            embed.embeds[0].title = `[${body.model.name}] A new label was added to ${"``" + body.action.data.card.name + "``"}`;
+            webhookBody.embeds[0].title = `[${body.model.name}] A new label was added to ${"``" + body.action.data.card.name + "``"}`;
             let description = `A \`\`${body.action.data.card.label}\`\` label was added to the ${"``" + body.action.data.card.name + "``"} card.`
-            embed.embeds[0].description = description;
+            webhookBody.embeds[0].description = description;
         }
+        webhookBody = JSON.stringify(webhookBody);
+        let postOptions = {
+            hostname: 'discordapp.com',
+            port: 443,
+            path: discordAddress,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(webhookBody)
+            },
+            body: webhookBody
+        }
+        https.request(postOptions, (response) => {
+            if (response.statusCode == 200 || response.statusCode == 204) {
+                res.statusCode(204).end();
+            } else {
+                console.log(response.statusCode);
+            }
+        });
     } else {
         res.status(400).json({});
         console.log(service);
